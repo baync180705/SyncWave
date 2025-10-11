9ec301f54d13b10aa983b0bb316e6f8c6c698487
Budhayan Chakraborty <budhayanc2005@gmail.com>
2025-10-11 16:17:27 +0530

    feat(routes): Add routes for adding and removing music from the track

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import cors from "cors";
 import corsConfig from "./config/corsConfig";
 import roomRoutes from "./routes/roomRoutes";
+import trackRoutes from "./routes/trackRoutes";
 
 const app: Application = express();
 
@@ -9,6 +10,7 @@ app.use(cors(corsConfig));
 app.use(express.json());
 
 app.use("/api/rooms", roomRoutes);
+app.use("/api/tracks", trackRoutes);
 
 
 export default app;
